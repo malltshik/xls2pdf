eca26d78b3d3ac44ea43b4f1cba77976dbbb9624
Artem Gavrilov <malltshik@gmail.com>
2018-05-25 18:02:07 +0300

    implement borderes and color for HSSFHtmlHelper

--- src/main/java/ru/malltshik/xls2pdf/helpers/impl/HSSFHtmlHelper.java
+++ src/main/java/ru/malltshik/xls2pdf/helpers/impl/HSSFHtmlHelper.java
@@ -6,9 +6,14 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.hssf.util.HSSFColor.HSSFColorPredefined;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Color;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
 import ru.malltshik.xls2pdf.helpers.HtmlHelper;
 
 import java.util.Formatter;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class HSSFHtmlHelper implements HtmlHelper {
 
@@ -44,8 +49,29 @@ public class HSSFHtmlHelper implements HtmlHelper {
         }
     }
 
-    //TODO implement this
     public void borderStyles(CellStyle style, Formatter out) {
+        HSSFCellStyle xstyle = (HSSFCellStyle) style;
+        styleOut("border-left", style.getBorderLeftEnum(), BORDER, xstyle.getLeftBorderColor(), out);
+        styleOut("border-right", style.getBorderRightEnum(), BORDER, xstyle.getRightBorderColor(), out);
+        styleOut("border-top", style.getBorderTopEnum(), BORDER, xstyle.getTopBorderColor(), out);
+        styleOut("border-bottom", style.getBorderBottomEnum(), BORDER, xstyle.getBottomBorderColor(), out);
+    }
 
+    private <K> void styleOut(String attr, K key, Map<K, String> mapping, short color, Formatter out) {
+        String value = mapping.get(key);
+        String c = borderStyle(color);
+        value = String.format(value, c);
+        if (value != null) {
+            out.format("  %s: %s;%n", attr, value);
+        }
+    }
+
+    private String borderStyle(short color) {
+        HSSFColorPredefined xcolor = Stream.of(HSSFColorPredefined.values())
+                .filter(x -> x.getIndex() == color).findFirst().orElse(HSSFColorPredefined.AUTOMATIC);
+        if (!xcolor.equals(HSSFColorPredefined.AUTOMATIC) && xcolor.getHexString() != null) {
+            return "#" + xcolor.getHexString().substring(2);
+        }
+        return AUTO_COLOR;
     }
 }
